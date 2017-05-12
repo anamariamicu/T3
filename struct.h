ed73aef029429f92ff49511d6b8a0894a0617187
georgebukkosi <georgebukkosi@gmail.com>
2017-05-12 15:01:24 +0300

    Add files via upload
    adaugat structuri de baza

---
+++ struct.h
@@ -0,0 +1,21 @@
+#ifndef __STRUCT_H__
+#define __STRUCT_H__
+
+struct movie {
+  std::string name;
+  std::string id;
+  int timestamp;
+  std::string director;
+};
+
+struct person {
+  std::string name;
+  std::string id;
+};
+
+struct actor {
+  std::string name;
+  std::string id;
+};
+
+#endif
